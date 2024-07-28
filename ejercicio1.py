731f7784d4ec8e634953a2f91b3c27fd6ac06735
PAOLA VERONICA GUAJARDO REYNAGA <pguajre@bci.cl>
2024-07-28 11:08:31 -0400

    termino ejercicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -16,4 +16,14 @@ df5.set_index('product_id' ,inplace=True)
 
 
 clientes_unicos = df1['customer_unique_id'].nunique() 
-print("los clientes unic s son: ",clientes_unicos)
+print("los clientes unicos son:",clientes_unicos)
+
+promedio= df3['payment_value'].mean()
+print("promedio de valor de pago por pedido",promedio)
+
+category=df5['product_category_name'].value_counts()
+la_mas_repetida=category.idxmax()
+print("el producto mas vendido es:", la_mas_repetida) 
+
+total_pedidos=len(df4)
+print("el total de pedidos realizados es:", total_pedidos)
